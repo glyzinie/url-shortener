b58f1dcac46ae142107189481518033737af8268
Wis <149991443+glyzinie@users.noreply.github.com>
2024-05-02 22:31:34 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import { Hono } from 'hono'
 
-// Honoアプリのインスタンスを作成
 const app = new Hono()
 
-// 短縮URLリダイレクト用のルート
-app.get('/:id', async (c) => {
-  const { id } = c.req.params
-
-  // KV ストアから URL を取得
-  const url = await URL_KV.get(id)
-  if (url) {
-    return c.redirect(url)
-  } else {
-    return c.text('URL not found', 404)
-  }
+app.get('/', (c) => {
+  return c.text('Hello, world!', 200)
 })
 
-// Honoアプリの起動
+ app.get('/:id', async (c) => {
+   const { id } = c.req.params
+
+   const url = await URL_KV.get(id)
+   if (url) {
+     return c.redirect(url)
+   } else {
+     return c.text('URL not found', 404)
+   }
+ })
+
+app.all('*', (c) => {
+  return c.text('Bad Request', 400)
+})
+
 app.fire()
